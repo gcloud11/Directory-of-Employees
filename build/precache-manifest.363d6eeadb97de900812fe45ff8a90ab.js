cf21bb97f2f739590dcd24c4229de8aa7e476c0a
Gayla Cloud <gaylacloud@gaylas-mbp.home>
2020-11-13 14:12:58 -0600

    Connecting server.js

---
+++ build/precache-manifest.363d6eeadb97de900812fe45ff8a90ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe90762680370efcf8c3a98c809c90a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e897cf0f3494e75f06fa",
+    "url": "/static/css/main.cf30b5ca.chunk.css"
+  },
+  {
+    "revision": "8149179c50fc1068d2d8",
+    "url": "/static/js/2.d385e262.chunk.js"
+  },
+  {
+    "revision": "a49b139e5f899b206ab86b6795fd5184",
+    "url": "/static/js/2.d385e262.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e897cf0f3494e75f06fa",
+    "url": "/static/js/main.0cc8ccbb.chunk.js"
+  },
+  {
+    "revision": "93dbc0720728f92ecbc4",
+    "url": "/static/js/runtime-main.78bf1add.js"
+  }
+]);
